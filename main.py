001fef786213a190f082c9576b7869a43f2877e9
makasim98 <maxluch98@gmail.com>
2025-07-13 14:45:46 -0400

    limit game to 60FPS

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
 
     pygame.init()
     display = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
        for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
 
        display.fill(color="black")
        pygame.display.flip()
+       dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
