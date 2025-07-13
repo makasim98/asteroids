b4d0eac2830b68b48f9ac96f0036e2a77c9d58ba
makasim98 <maxluch98@gmail.com>
2025-07-13 15:00:14 -0400

    imlpement player sprite and draw it on the screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -11,14 +12,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    init_x, init_y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
+    player = Player(init_x, init_y)
+
     while True:
-       for event in pygame.event.get():
+        for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
 
-       display.fill(color="black")
-       pygame.display.flip()
-       dt = clock.tick(60) / 1000
+        display.fill(color="black")
+        player.draw(display)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
