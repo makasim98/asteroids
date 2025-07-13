c40c95500110f62b716e1b3f702ad3025f985c3d
makasim98 <maxluch98@gmail.com>
2025-07-13 16:15:39 -0400

    Add shooting mechanic to the player

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -17,24 +18,34 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     init_x, init_y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
     player = Player(init_x, init_y)
     asteroid_field = AsteroidField()
 
     while True:
+        # Check for inputs
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
                return
 
+        # Update Screen
         display.fill(color="black")
         updatable.update(dt)
         for member in drawable:
             member.draw(display)
 
+        for asteroid in asteroids:
+            if asteroid.colides_with(player):
+                print("Game Over!")
+                return
+
+        # Show next frame
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
